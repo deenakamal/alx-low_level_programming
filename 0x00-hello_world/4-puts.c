737f7c3bdf72a0bfb129261635bb71c86bb4f81f
“Deena <deenah.kamal@gmail.com>
2023-06-18 16:42:36 -0700

    Hello, puts commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print statement without printf
+ *
+ * Return: alwayes (0) success
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
