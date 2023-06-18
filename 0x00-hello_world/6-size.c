307c9905692e849f09932549668e996d0e246e10
“Deena <deenah.kamal@gmail.com>
2023-06-18 16:57:45 -0700

    commit c file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print the size of different types
+ *
+ * Return: Alwayes 0 (Success)
+*/
+
+int main(void)
+{
+	printf("Size of a char: %d byte(S)\n", sizeof(char));
+	printf("Size of an int: %d byte(S)\n", sizeof(int));
+	printf("Size of a long int: %d byte(S)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(S)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(S)\n", sizeof(float));
+	return (0);
+}
