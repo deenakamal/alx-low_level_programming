dbc838f46d6e52fc5629486ba7a3e5a350873874
“Deena <deenah.kamal@gmail.com>
2023-06-18 19:36:06 -0700

    final

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print all single digit
+ *
+ * Return: Alwayes 0 (Success)
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
