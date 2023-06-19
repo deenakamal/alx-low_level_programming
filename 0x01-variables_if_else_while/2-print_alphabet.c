d6e400a146c4ed6142a0629348c49d5458f11882
“Deena <deenah.kamal@gmail.com>
2023-06-18 18:28:37 -0700

    alphabet commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: generate character print alphabet
+ *
+ * Return: Alwayes 0 (success)
+*/
+int main(void)
+{
+	char alphabet = 'a';
+
+	while ('a' <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++
+	}
+	putchar('\n');
+	return (0);
