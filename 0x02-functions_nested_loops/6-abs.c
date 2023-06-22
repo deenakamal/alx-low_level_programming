5c967c34171ba009fb67cf84e9f30472e0f140c2
“Deena <deenah.kamal@gmail.com>
2023-06-21 19:37:34 -0700

    d

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value
+ *
+ * @n: input integer
+ *
+ * Return: integer
+*/
+
+int _abs(int n)
+{
+	if (n <= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		n = -n;
+	}
+	return (n)
+}
