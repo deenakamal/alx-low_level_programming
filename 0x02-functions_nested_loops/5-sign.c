3c158923a47896f64012d43defc5567ac67d5238
“Deena <deenah.kamal@gmail.com>
2023-06-21 19:31:47 -0700

    f

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - function to print sign of number
+ *
+ * @n: input number
+ *
+ * Return: 1 when n > 0, 0 when n=0, -1 when n<0
+ *
+*/
+
+int print_sign(int n)
+{
+	if n > 0
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if n == 0
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
