3faf32fdcc8803fb460a7dbbcaf611fc0120fddb
“Deena <deenah.kamal@gmail.com>
2023-06-21 19:06:19 -0700

    second commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - function print alphabet
+ *
+*/
+
+void print_alphabet(void)
+{
+	int ch = 'a';
+
+	while (cha <= 'z')
+	{
+		_putchar(ch);
+		ch++
+	}
+	_putchar('\n');
+}
