18b91e3120dca8c90ca9651fc9e0870a993c78ff
“Deena <deenah.kamal@gmail.com>
2023-06-21 19:44:29 -0700

    d

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: sum of input 1 and input 2
+*/
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
