a1bed38ae67bee45e0d66772657dfdfc8007c361
“Deena <deenah.kamal@gmail.com>
2023-06-26 19:24:40 -0700

    m

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse
+ *
+ * @s: input string
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	while (i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
