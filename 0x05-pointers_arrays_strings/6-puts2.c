3c68c3248fdd1788b819376f7f980e7112346037
“Deena <deenah.kamal@gmail.com>
2023-06-26 19:51:53 -0700

    m'

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - function print number divided on 2
+ * @str: input string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
