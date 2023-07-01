fa1b2fd7a9b4461ec228137ce20686c65dc8c9a8
“Deena <deenah.kamal@gmail.com>
2023-06-30 19:14:39 -0700

    commit git push

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,15 +7,16 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int c1, c2;
+	int c1 = 0;
+	int c2;
 
-	for (c1 = 0; dest[c1]; c1++)
-		;
+	while (dest[c1])
+		c1++;
 	/*iterate through array to get value without null*/
 	for (c2 = 0; src[c2]; c2++)
 	{
 		dest[c1] = src[c2];
-		c1++
+		c1++;
 	}
 	return (dest);
 }
