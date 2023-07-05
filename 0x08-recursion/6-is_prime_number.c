c15d6ebd8a98f6bb9cf77696886b7fb3370cb716
“Deena <deenah.kamal@gmail.com>
2023-07-05 12:02:57 -0700

    m

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,9 +9,9 @@ int is_prime_number(int n)
 	if (n <= 1)
 		return (0);
 	if (n == 1)
-		return (1);
-	if (n % n == 0 && n > 0)
 		return (0);
+	if (n % n == 0 && n > 0)
+		return (1);
 	return (is_prime_number(n - 1));
 }
 
