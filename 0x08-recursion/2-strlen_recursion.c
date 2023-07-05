205e6eddc665ae7bff5dc221980e07aa63703403
“Deena <deenah.kamal@gmail.com>
2023-07-05 10:18:42 -0700

    m

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - get the length of input string
+ *
+ * @s: input character
+ *
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	int l;
+
+	if (*s > '\0')
+	{
+		l++;
+		l = l + _strlen_recursion(s + 1);
+	}
+}
