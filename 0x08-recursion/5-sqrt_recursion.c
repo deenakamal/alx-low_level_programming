4ea7df235aba0d9552995cdc3860ce60b620d456
“Deena <deenah.kamal@gmail.com>
2023-07-05 11:28:19 -0700

    m

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-int _sqr(int n2, value);
+int _sqr(int n2, int value);
 /**
  * _sqrt_recursion - return square
  * @n: input number
@@ -15,11 +15,11 @@ int _sqrt_recursion(int n)
  * @value: squre root
  * Return: number
  */
-int _sqr(int n2, value)
+int _sqr(int n2, int value)
 {
-	if (value *value < n2)
+	if (value * value < n2)
 		return (_sqr(n, value + 1));
-	else if (value *value == n2)
+	else if (value * value == n2)
 		return (value);
 	else
 		return (-1);
