5975a9cadd9dc2833ceaefeb512452ae5de37e81
“Deena <deenah.kamal@gmail.com>
2023-07-05 12:01:05 -0700

    m

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * is_prime_number - get primew
+ * @n: number
+ * Return:1 if the number is prime number
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	if (n == 1)
+		return (1);
+	if (n % n == 0 && n > 0)
+		return (0);
+	return (is_prime_number(n - 1));
+}
+
