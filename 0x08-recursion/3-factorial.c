be8d4ed64491ba05a1f82463fdc855c479d29210
“Deena <deenah.kamal@gmail.com>
2023-07-05 10:25:55 -0700

    m

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * factorial - get facrotial of given number
+ *
+ * @n: input number
+ *
+ * Return: if n < 0 return -1
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
