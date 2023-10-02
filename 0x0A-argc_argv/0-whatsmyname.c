af94eee2d5146965604df2ae0ae9698e54aaf1f4
“Deena <deenah.kamal@gmail.com>
2023-10-02 15:25:11 -0700

    commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include "main.h"
 /**
  * main - a program that prints its name, followed by a new line.
  *
@@ -6,7 +7,7 @@
  * @argv: array
  * Return: 0
  */
-int main(int argc, char const argv[])
+int main(int argc, char const *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
