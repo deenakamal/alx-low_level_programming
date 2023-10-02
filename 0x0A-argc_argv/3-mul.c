442bb7f904f6086299e748169d563bf5b951f566
“Deena <deenah.kamal@gmail.com>
2023-10-02 15:45:51 -0700

    commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Write a program that multiplies two numbers.
+ * @argc: input number
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int f_num, s_num;
+
+	f_num = atoi(argv[1]);
+	s_num = atoi(argv[2]);
+	result = f_num * s_num2;
+
+	printf("%d\n", result);
+	return (0);
+}
