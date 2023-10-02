8ed2a1148fc83cab815057ab1e3e4300ad947fdc
“Deena <deenah.kamal@gmail.com>
2023-10-02 13:43:01 -0700

    m

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: input 1
+ * @needle: input 2
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (0);
+}
