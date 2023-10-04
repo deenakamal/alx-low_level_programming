94fd16a977949003496be7dff9d6afdc9f59a0e1
“Deena <deenah.kamal@gmail.com>
2023-10-04 07:34:33 -0700

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,15 +9,15 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	int length1 = 0;
 	char *newResult;
+	int length2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 	/* find the length of string */
-	int length1 = 0;
-	int length2 = 0;
 
 	while (s1[length1] != '\0')
 		length1++;
@@ -25,6 +25,9 @@ char *str_concat(char *s1, char *s2)
 		length2++;
 
 	newResult = malloc((length1 + length2 + 1) * sizeof(char));
+
+	if (newResult == NULL)
+		return (NULL);
 
 	while (s1[length1] != '\0')
 	{
