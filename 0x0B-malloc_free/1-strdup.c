81d17762c2b7287afdfa087a7d070f5decb66fd3
“Deena <deenah.kamal@gmail.com>
2023-10-04 07:04:57 -0700

    commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - copy of the string given as a parameter.
+ * @str: char input
+ * Return: NULL if str = NULL
+ */
+char *_strdup(char *str)
+{
+	int i;
+	int length = 0;
+	char *array;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[length] != '\0')
+		length++;
+	array = malloc(sizeof(char) * (length + 1));
+
+	if (array == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		array[i] = str[i];
+		i++;
+	}
+	return (array);
+}
