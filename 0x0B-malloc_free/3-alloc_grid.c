7d102a2d61a481bb86b3d658c843a32ab47df639
“Deena <deenah.kamal@gmail.com>
2023-10-04 08:14:24 -0700

    commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: width of array
+ * @height: height of array
+ * Return: NULL on failure
+ */
+int **alloc_grid(int width, int height)
+{
+	/* creat double pointer one for row and one for colum */
+
+	int *p;
+	int i;
+	int j;
+
+	p = malloc(sizeof(*p) * height);
+
+	if (width <= 0 || height <= 0 || p == 0)
+	{
+
+		return (NULL);
+	}
+	else
+	{
+		for (int i = 0; i < height; i++)
+		{
+			p[i] = malloc(sizeof(**p) * width);
+			if (p[i] == NULL)
+			{
+				for (; i > = 0 ; i--)
+					free(p[i]);
+				free(p);
+				return (NULL);
+			}
+			for (j = 0; j < width; j++)
+				p[i][j] = 0;
+		}
+	}
+	return (p);
+}
