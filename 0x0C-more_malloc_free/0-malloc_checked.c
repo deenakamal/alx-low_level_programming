6d7b12e2f013cb7bb693caae8179e4526394cc5a
“Deena <deenah.kamal@gmail.com>
2023-10-07 07:54:51 -0700

    commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,10 +2,12 @@
 /**
  * malloc_checked - allocate a single large block of memory
  * @b: size of value to allocate in memory
+ *
+ * Return: pointer to allocate memory
  */
 void *malloc_checked(unsigned int b)
 {
-	int *p;
+	unsigned int *p;
 
 	p = malloc(b);
 
