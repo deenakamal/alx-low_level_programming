bc6d85039a065be094c0e5a93bc0af0dc17fc36e
“Deena <deenah.kamal@gmail.com>
2023-10-07 08:47:22 -0700

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,7 +5,7 @@
  * @s2: second string
  * @n: number bytes of s2
  *
- * Return: return pointer to a newly allocated space in th memory
+ * Return: return pointer to newly allocated space in th memory
  * If the function fails, it should return NULL
  * If n is greater or equal to the length of s2 then use the entire string s2
  * if NULL is passed, treat it as an empty string
