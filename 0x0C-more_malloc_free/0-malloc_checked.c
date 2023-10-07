e1dcee5665c0def22199ae9a3c7bea05fa4d4817
“Deena <deenah.kamal@gmail.com>
2023-10-07 07:48:42 -0700

    commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * malloc_checked - allocate a single large block of memory
+ * @b: size of value to allocate in memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+Readme file for project Automatic and dynamic allocation, malloc and free
