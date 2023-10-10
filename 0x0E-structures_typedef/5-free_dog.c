8855756f8d10d19701363392f4ff03cda9be22c7
“Deena <deenah.kamal@gmail.com>
2023-10-10 16:29:38 -0700

    commit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - free memory allocated for dogs
+ * @d: struct dog to free
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
