128e7de2f72b5f5d924c41cb5b850bfc0c2b9243
“Deena <deenah.kamal@gmail.com>
2023-10-10 12:39:58 -0700

    commit

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+Structures, typedef Project
+Tasks:
+0. Poppy
+1. A dog is the only thing on earth that loves you more than you love yourself
+3.A dog will teach you unconditional love. If you can have that in your life, things won't be too bad
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifdef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * owner: the owner of the dog
+ * Description: longer description for struct dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
