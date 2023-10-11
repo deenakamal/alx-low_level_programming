6e2e49579b5f058694842f29e672b85393f81c57
“Deena <deenah.kamal@gmail.com>
2023-10-11 14:05:59 -0700

    commit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,10 +22,12 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (ops[i].op != NULL)
 	{
+		if (s[0] == ops[i].op[0])
+			return (ops[i].f);
 		i++;
 	}
 
-	return (ops[i].f);
+	return (NULL);
 }
