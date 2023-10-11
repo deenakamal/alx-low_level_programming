6b9dae59a581a3fee7d92ed321b9f0d9d098a618
“Deena <deenah.kamal@gmail.com>
2023-10-11 12:42:47 -0700

    commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,7 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array != NULL && cmp != NULL && size > 0)
 	{
-		for (i = 0 ; i < 0; i++)
+		for (i = 0 ; i < size; i++)
 		{
 			if (cmp(array[i]))
 				return (i);
