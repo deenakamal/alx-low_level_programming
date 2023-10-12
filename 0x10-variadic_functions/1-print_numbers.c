ac7f40c6d576bedb990b8d2d5502728c6790622d
“Deena <deenah.kamal@gmail.com>
2023-10-12 07:38:26 -0700

    commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,15 +9,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	/* numbers pointer point to all list */
 	va_list numbers;
-	int i = n;
+	unsigned int i = 0;
 
 	va_start(numbers, n);
 
-	while (i--)
+	while (i < n)
 	{
 		printf("%d", va_arg(numbers, int));
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
+		i++;
 	}
 	printf("\n");
 	va_end(numbers);
