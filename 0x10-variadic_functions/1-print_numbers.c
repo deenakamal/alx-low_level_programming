3b6718edc8dc91811f1cb19d46444d23ed08478b
“Deena <deenah.kamal@gmail.com>
2023-10-12 07:29:29 -0700

    commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	while (i--)
 	{
 		printf("%d", va_arg(numbers, int));
-		if (i != n - 1 && separator != NULL)
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
