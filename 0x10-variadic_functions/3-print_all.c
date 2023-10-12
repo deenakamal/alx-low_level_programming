a237c103d80add29568b494ac14c6f5282481bb4
“Deena <deenah.kamal@gmail.com>
2023-10-12 16:12:19 -0700

    commit

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+/**
+ * print_all - used to print anything
+ * @format: list of arguments
+ * @...: arguments to func
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list prm;
+	int i;
+	float f;
+	char *s;
+	char *_sep = "";
+
+	va_start(prm, format);
+	i = 0;
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", _sep, va_arg(prm, int));
+				break;
+			case 'i':
+				printf("%s%d", _sep, va_arg(prm, int));
+				break;
+			case 'f':
+				f = va_arg(prm, double);
+				printf("%s%f", _sep, f);
+				break;
+			case 's':
+				s = va_arg(prm, char*);
+				if (!s)
+					printf("(nil)");
+				printf("%s%s", _sep, s);
+				break;
+			default:
+				i++;
+				continue;
+			}
+			_sep = ", ";
+			i++;
+	}
+	printf("\n");
+	va_end(prm);
+}
