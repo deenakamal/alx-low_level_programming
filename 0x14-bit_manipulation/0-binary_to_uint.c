59fa956e8560440c924e7391f100306ec89d7bfd
deenakamal <deenah.kamal@gmail.com>
2023-10-26 14:08:47 -0700

    commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * binary_to_uint - function that converts a binary to an unsigned int
+ *
+ * @b: pointer to a string
+ *
+ * Return: the converted number, or 0 if
+ * there is one or more chars in the string b that is not 0 or 1
+ * b is NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int converted_number = 0;
+	int index;
+
+	for (index = 0; b[index] != '\0'; index++)
+	{
+		if (b[index] != '0' && b[index] != '1')
+			return (0);
+		converted_number = converted_number << 1;
+
+		if (b[index] == '1')
+			converted_number |= 1;
+	}
+	return (converted_number);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,3 @@
+Bit manipulation project
+
+Tasks:
