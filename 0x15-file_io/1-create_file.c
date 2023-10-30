4e5f93ed0562d6086e302451587052ddafec5518
deenakamal <deenah.kamal@gmail.com>
2023-10-30 11:22:31 -0700

    creat file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-	if (text_contant == NULL)
+	if (text_content == NULL)
 		text_content = "";
 
 	while (text_content[len] != '\0')
