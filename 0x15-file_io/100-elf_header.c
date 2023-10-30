80822d166331adf86564c6e5d476f8cb8134752d
deenakamal <deenah.kamal@gmail.com>
2023-10-30 12:42:24 -0700

    init

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <elf.h>
+/**
+ * main - program to check
+ * @argc: argument number
+ * @argv: argument value
+ * Return: 1 on succeaa otherwise 0
+ */
+int main(int argc, char **argv)
+{
+	int fd;
+	Elf46_Ehdr *header;
+	ssize_t num_butes;
+
+	fd = open (argv[1], O_RDONLY);
+	if (fd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	if (argc != 2)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	header = malloc(sizeof(Elf46_Ehdr));
+	if (!header)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+
+	return (EXIT_SUCCESS);
+}
