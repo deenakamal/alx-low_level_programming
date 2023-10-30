312a1260a74006ab33c7b2bf76f46057955209ed
deenakamal <deenah.kamal@gmail.com>
2023-10-30 11:19:53 -0700

    creat_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - creat a function this function creat file
+ *
+ * @filename: file name
+ * @text_content: text to write into file
+ *
+ * Return: 1 on success and -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t len = 0, num_bytes;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+	if (text_contant == NULL)
+		text_content = "";
+
+	while (text_content[len] != '\0')
+		len++;
+
+	num_bytes = write(fd, text_content, len);
+
+	if (num_bytes == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
