1508b67470dc6fed0b2910e43abd30c54abeb52f
deenakamal <deenah.kamal@gmail.com>
2023-10-30 11:10:14 -0700

    resd file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,18 +11,23 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t num_read_write, fd;
-	char buff[BUFFERSIZE * 8];
+	char *buff;
+
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
+		return (0);
 
 	if (!filename)
 		return (0);
 
-	fd = open(filename, O_READONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	num_read_write = read(fd, &buff[0], letters);
-	num_read_write = write(STDOUT_FILED, &buff[0], num_read_write);
+	num_read_write = write(STDOUT_FILENO, &buff[0], num_read_write);
 
 	close(fd);
+	free(buff);
 
 	return (num_read_write);
 }
