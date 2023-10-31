d828ed7a3ec300891beee6e19f4e6505c6f120cd
deenakamal <deenah.kamal@gmail.com>
2023-10-31 15:18:09 -0700

    edit elf betty

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -19,14 +19,23 @@ int main(int argc, char **argv)
 	int fd;
 	Elf64_Ehdr *h;
 
-	fd = open (argv[1], O_RDONLY);
+	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	if (argc != 2)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	h = malloc(sizeof(Elf64_Ehdr));
 	if (!h)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
 	return (0);
 }
